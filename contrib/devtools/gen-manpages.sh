6990321197e2763f574be6dc05cb1f3c0ed37b3c
Harris Brakmic <brakmic@gmail.com>
2019-08-16 14:02:15 +0200

    move to ACM

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,11 +7,11 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$BINDIR/bitcoind}
-BITCOINCLI=${BITCOINCLI:-$BINDIR/bitcoin-cli}
-BITCOINTX=${BITCOINTX:-$BINDIR/bitcoin-tx}
-WALLET_TOOL=${WALLET_TOOL:-$BINDIR/bitcoin-wallet}
-BITCOINQT=${BITCOINQT:-$BINDIR/qt/bitcoin-qt}
+BITCOIND=${BITCOIND:-$BINDIR/Actiniumd}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/Actinium-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/Actinium-tx}
+WALLET_TOOL=${WALLET_TOOL:-$BINDIR/Actinium-wallet}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/Actinium-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 
